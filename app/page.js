c075eb6623a39f0f9ca8c5b6c93adddbf89ef99f
Abdul Malik <83514434+abdulmalik900@users.noreply.github.com>
2023-10-27 02:32:37 -0700

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+    
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    </>
+  )
+}
